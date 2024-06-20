fedd8e083a08792ba017914666859d567f8c3455
Thomas Shaw <printer.83mph@gmail.com>
2024-06-20 16:02:32 -0700

    Hotfix: disabled secondary button in dark mode

--- web/src/components/common/button-styles.tsx
+++ web/src/components/common/button-styles.tsx
@@ -7,5 +7,5 @@ export const buttonVariants = {
   primary:
     'bg-primary-500 text-white shadow hover:bg-primary-600 disabled:opacity-70',
   secondary:
-    'bg-white text-primary-500 ring-1 ring-gray-200 hover:bg-gray-100 disabled:bg-gray-50 disabled:text-primary-500/60 dark:bg-neutral-700 dark:text-neutral-200 dark:ring-2 dark:ring-neutral-500 dark:hover:bg-neutral-600',
+    'bg-white text-primary-500 ring-1 ring-gray-200 hover:bg-gray-100 disabled:bg-gray-50 disabled:text-primary-500/60 dark:bg-neutral-700 dark:text-neutral-200 dark:ring-2 dark:ring-neutral-500 dark:hover:bg-neutral-600 dark:disabled:bg-neutral-600 dark:disabled:text-neutral-400',
 };
